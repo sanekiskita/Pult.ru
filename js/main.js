532f80b0f0b85ff5ed1ab7a210f8e6bbf2739a7f
sanekiskita <as1405503@gmail.com>
2021-04-05 10:13:16 +0300

    init: start task

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+$(function () {
+    let includes = $('[data-include]')
+    $.each(includes, function () {
+      let file = './' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+  });
+
+
+
+$('.shopmenu__href').hover(function(){
+  $(this).addClass("active");
+  $('.shopmenu__img').attr('src', '/assets/img/shopmenu/' +$(this).data('img'));
+  console.log('/assets/img/shopmenu/' +$(this).data('img'));
+},function(){
+  $(this).removeClass("active");
+}
+);
+
+  $(document).ready(function(){
+    $('.slick1').slick({
+      dots: true,
+      infinite: true,
+      arrows: false,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      rows: 0
+    });
+    $('.slick2').slick({
+      dots: true,
+      infinite: true,
+      arrows: false,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      rows: 0
+    });
+  });
